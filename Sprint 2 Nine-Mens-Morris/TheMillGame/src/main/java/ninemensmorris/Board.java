b0db8164bee09cb5349b7341e7dda6ef70e60782
AldoLunaBueno <alunab@uni.pe>
2022-11-03 01:20:38 -0500

    Inicializaciones

--- Sprint 2 Nine-Mens-Morris/TheMillGame/src/main/java/ninemensmorris/Board.java
+++ Sprint 2 Nine-Mens-Morris/TheMillGame/src/main/java/ninemensmorris/Board.java
@@ -5,7 +5,7 @@ public class Board {
 
     public int[] boardPosition ;
     public int[][] millCombinations;
-
+    public int[][] boardPositionValues;
 
     static public final int numberOfPositionsOnBoard = 24;
     static public final int numberOfMillCombinations = 16;
@@ -13,6 +13,7 @@ public class Board {
     public String currentTurn ;
     private int numPlayers = 2 ;
     private int[] numberOfPiecesOfPlayer ;
+
 
     public void Board(){
         boardPosition = new int[Board.numberOfPositionsOnBoard];
@@ -66,10 +67,47 @@ public class Board {
     }
 
     public void initBoard(){
-
+        boardPosition = new int[]{
+                0,          3,          6,
+                    8,      10,     12,
+                        16, 17, 18,
+                21, 22, 23,     25, 26, 27,
+                        30, 31, 32,
+                    36,     38,     40,
+                42,         45,         48
+        };
+        boardPositionValues = new int[][]{
+                {0, -1, -1, 0, -1, -1, 0},
+                {-1, 0, -1, 0, -1, 0, -1},
+                {-1, -1, 0, 0, 0, -1, -1},
+                {0, 0, 0, -1, 0, 0, 0},
+                {-1, -1, 0, 0, 0, -1, -1},
+                {-1, 0, -1, 0, -1, 0, -1},
+                {0, -1, -1, 0, -1, -1, 0},
+        };
     }
     public void initMillCombinations(){
+        millCombinations = new int[][]{
+                // Horizontales
+                {0, 3, 6},
+                {8, 10, 12},
+                {16, 17, 18},
+                {21, 22, 23},
+                {25, 26, 27},
+                {30, 31, 32},
+                {36, 38, 40},
+                {42, 45, 48},
 
+                // Verticales
+                {0, 21, 42},
+                {8, 22, 36},
+                {16, 23, 30},
+                {3, 10, 17},
+                {31, 38, 45},
+                {18, 25, 32},
+                {12, 26, 40},
+                {6, 27, 48}
+        };
     }
 
 
